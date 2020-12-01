8d068e3b5491e82ce8dbe334f8d0e9816b8881bb
Alexander Lavrov <avlavrov@yandex.ru>
2020-12-02 03:49:47 +0400

    Sprint 4. Rev 1. V1. before js

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,20 @@ let closePopupButton = document.querySelector('.popup__container-close');
 let savePopupButton = document.querySelector('.popup__button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
+let popup = document.querySelector('.popup');
 
 function openPopup() {
-  let popup = document.querySelector('.popup');
   popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-  let popup = document.querySelector('.popup');
   popup.classList.remove('popup_opened');
 }
 
+let newProfileName = popup.querySelector('.popup__name');
+let newProfileJob = popup.querySelector('.popup__job');
+
 function saveProfile() {
-  let popup = document.querySelector('.popup');
-  let newProfileName = popup.querySelector('.popup__name');
-  let newProfileJob = popup.querySelector('.popup__job');
   popup.classList.remove('popup_opened');
   profileName.textContent = newProfileName.value;
   profileJob.textContent = newProfileJob.value;
