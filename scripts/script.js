0e1e35d64a3afec5ed386bbbbafb69287e01bdb3
Alexander Lavrov <avlavrov@yandex.ru>
2020-12-02 05:08:07 +0400

    Sprint 4. Rev.1 V.2

--- scripts/script.js
+++ scripts/script.js
@@ -4,24 +4,21 @@ let savePopupButton = document.querySelector('.popup__button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let popup = document.querySelector('.popup');
+  let newProfileName = popup.querySelector('.popup__name');
+  let newProfileJob = popup.querySelector('.popup__job');
+let formElement = popup.querySelector('.popup__form');
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+function changePopup() {
+  popup.classList.toggle('popup_opened');
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-
-let newProfileName = popup.querySelector('.popup__name');
-let newProfileJob = popup.querySelector('.popup__job');
-
-function saveProfile() {
-  popup.classList.remove('popup_opened');
+function saveProfile(evt) {
+  evt.preventDefault();
+  changePopup();
   profileName.textContent = newProfileName.value;
   profileJob.textContent = newProfileJob.value;
 }
 
-editButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
-savePopupButton.addEventListener('click', saveProfile);
+editButton.addEventListener('click', changePopup);
+closePopupButton.addEventListener('click', changePopup);
+formElement.addEventListener('submit', saveProfile);
