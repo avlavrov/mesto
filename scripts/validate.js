8f1703c33df0283a278806bf4e571dd25fab79f6
Alexander Lavrov <avlavrov@yandex.ru>
2020-12-26 03:59:54 +0400

    Sprint6. Rev1.

--- scripts/validate.js
+++ scripts/validate.js
@@ -4,7 +4,7 @@ const config = {
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+  errorClass: 'popup__error-message'
 }
 
 function hideInputError(formElement, inputElement) {
